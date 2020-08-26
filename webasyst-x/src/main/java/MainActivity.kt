5b369e5a66c4ac356e47f23c8ed660fb3748414b
Nikita Danilov <mirt@mirt.su>
2020-08-26 15:06:56 +0300

    Fix authentication flow bugs
    https://1312.io/webasyst/tasks/#/task/7.338/

--- webasyst-x/src/main/java/MainActivity.kt
+++ webasyst-x/src/main/java/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.webasyst.x
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.webasyst.x.auth.WebasystAuthService
@@ -9,21 +11,40 @@ import kotlinx.android.synthetic.main.activity_main.refreshTokenTextView
 import kotlinx.android.synthetic.main.activity_main.tokenExpirationTextView
 import kotlinx.android.synthetic.main.activity_main.tokenTextView
 import net.openid.appauth.AuthState
+import net.openid.appauth.AuthorizationException
+import net.openid.appauth.AuthorizationRequest
+import net.openid.appauth.AuthorizationResponse
+import net.openid.appauth.AuthorizationServiceDiscovery
 import java.util.Date
 
 class MainActivity : AppCompatActivity(), WebasystAuthStateManager.AuthStateObserver {
     private val stateStore by lazy(LazyThreadSafetyMode.NONE) {
         WebasystAuthStateManager.getInstance(this)
     }
+    val webasystAuthService by lazy(LazyThreadSafetyMode.NONE) {
+        WebasystAuthService.getInstance(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        login.setOnClickListener {
-            val webasystAuthService = WebasystAuthService.getInstance(it.context)
-            webasystAuthService.authorize(this)
+        val intent = intent
+        if (null != intent) {
+            when (intent.action) {
+                ACTION_UPDATE_AFTER_AUTHORIZATION -> {
+                    val response = AuthorizationResponse.fromIntent(intent)
+                    val e = AuthorizationException.fromIntent(intent)
+                    stateStore.updateAfterAuthorization(response, e)
+
+                    if (null != response) {
+                        WebasystAuthService.getInstance(this).performTokenRequest(response.createTokenExchangeRequest())
+                    }
+                }
+            }
         }
+
+        login.setOnClickListener { authorize() }
     }
 
     override fun onResume() {
@@ -41,4 +62,27 @@ class MainActivity : AppCompatActivity(), WebasystAuthStateManager.AuthStateObse
         tokenExpirationTextView.text = state.accessTokenExpirationTime?.let { Date(it).toString() }
         refreshTokenTextView.text = state.refreshToken
     }
+
+    private fun authorize() {
+        val authRequest = webasystAuthService.createAuthorizationRequest()
+        val intent = createPostAuthorizationIntent(authRequest, null)
+        webasystAuthService.authorize(authRequest, intent)
+    }
+
+    private fun createPostAuthorizationIntent(
+        request: AuthorizationRequest,
+        discoveryDoc: AuthorizationServiceDiscovery?
+    ) : PendingIntent {
+        val intent = Intent(this, this.javaClass)
+        intent.action = ACTION_UPDATE_AFTER_AUTHORIZATION
+        if (null != discoveryDoc) {
+            intent.putExtra(WebasystAuthService.EXTRA_AUTH_SERVICE_DISCOVERY, discoveryDoc.docJson.toString())
+        }
+        return PendingIntent.getActivity(this, request.hashCode(), intent, 0)
+    }
+
+    companion object {
+        internal const val ACTION_UPDATE_AFTER_AUTHORIZATION = "update_post_auth"
+    }
+
 }
--- webasyst-x/src/main/java/auth/WebasystAuthService.kt
+++ webasyst-x/src/main/java/auth/WebasystAuthService.kt
@@ -2,23 +2,22 @@ package com.webasyst.x.auth
 
 import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import com.webasyst.x.util.SingletonHolder
+import kotlinx.coroutines.runBlocking
 import net.openid.appauth.AuthState
 import net.openid.appauth.AuthorizationRequest
 import net.openid.appauth.AuthorizationService
 import net.openid.appauth.AuthorizationServiceConfiguration
-import net.openid.appauth.AuthorizationServiceDiscovery
 import net.openid.appauth.CodeVerifierUtil
 import net.openid.appauth.ResponseTypeValues
 import net.openid.appauth.TokenRequest
 
-class WebasystAuthService private constructor(
+class WebasystAuthService internal constructor(
     context: Context,
     private val clientId: String,
-    private val authEndpoint: String,
-    private val tokenEndpoint: String,
+    authEndpoint: String,
+    tokenEndpoint: String,
     callbackUri: String,
 ) : WebasystAuthStateManager.AuthStateObserver {
     private val callbackUri = Uri.parse(callbackUri)
@@ -35,42 +34,26 @@ class WebasystAuthService private constructor(
     }
 
     override fun onChange(state: AuthState) {
-        println(state)
         if (state.needsTokenRefresh && state.refreshToken != null) {
             performTokenRequest(state.createTokenRefreshRequest())
         }
     }
 
-    fun authorize(context: Context) {
-        val codeVerifier = CodeVerifierUtil.generateRandomCodeVerifier()
+    fun authorize(authRequest: AuthorizationRequest, pi: PendingIntent) {
+        authService.performAuthorizationRequest(authRequest, pi)
+    }
 
-        val authRequest = AuthorizationRequest.Builder(
+    fun createAuthorizationRequest(): AuthorizationRequest {
+        val codeVerivier = CodeVerifierUtil.generateRandomCodeVerifier()
+
+        return AuthorizationRequest.Builder(
             authServiceConfiguration,
             clientId,
             ResponseTypeValues.CODE,
             callbackUri
         )
-            .setCodeVerifier(codeVerifier)
+            .setCodeVerifier(codeVerivier)
             .build()
-
-        authService.performAuthorizationRequest(authRequest, createPostAuthorizationIntent(
-            context,
-            authRequest,
-            authServiceConfiguration.discoveryDoc
-        ))
-    }
-
-    private fun createPostAuthorizationIntent(
-        context: Context,
-        request: AuthorizationRequest,
-        discoveryDoc: AuthorizationServiceDiscovery?
-    ) : PendingIntent {
-        val intent = Intent(context, WebasystAuthHelperService::class.java)
-        intent.action = WebasystAuthHelperService.ACTION_UPDATE_AFTER_AUTHORIZATION
-        if (null != discoveryDoc) {
-            intent.putExtra(EXTRA_AUTH_SERVICE_DISCOVERY, discoveryDoc.docJson.toString())
-        }
-        return PendingIntent.getService(context, request.hashCode(), intent, 0)
     }
 
     fun performTokenRequest(request: TokenRequest) {
@@ -79,19 +62,28 @@ class WebasystAuthService private constructor(
         }
     }
 
-    companion object : SingletonHolder<WebasystAuthService, Context>(Companion::create) {
+    suspend fun withFreshToken(block: suspend (accessToken: String?) -> Unit) {
+        this.stateStore.getCurrent().performActionWithFreshTokens(authService) { accessToken, idToken, ex ->
+            runBlocking {
+                block(accessToken)
+            }
+        }
+    }
+
+    companion object : SingletonHolder<WebasystAuthService, Context>(::createWebasystAuthService) {
         const val EXTRA_AUTH_SERVICE_DISCOVERY = "authServiceDiscovery"
 
-        private val configuration = WebasystAuthConfiguration()
+        internal val configuration = WebasystAuthConfiguration()
         fun configure(block: WebasystAuthConfiguration.() -> Unit) {
             configuration.apply(block)
         }
-        private fun create(context: Context): WebasystAuthService = WebasystAuthService(
-            context = context.applicationContext,
-            clientId = configuration.clientId ?: throw IllegalStateException("Client id must be set"),
-            authEndpoint = configuration.authEndpoint ?: throw IllegalStateException("Auth endpoint must be set"),
-            tokenEndpoint = configuration.tokenEndpoint ?: throw IllegalStateException("Token endpoint must be set"),
-            callbackUri = configuration.callbackUri ?: throw IllegalStateException("Callback uri must be set")
-        )
     }
 }
+
+private fun createWebasystAuthService(context: Context): WebasystAuthService = WebasystAuthService(
+    context = context,
+    clientId = WebasystAuthService.configuration.clientId ?: throw IllegalStateException("Client id must be set"),
+    authEndpoint = WebasystAuthService.configuration.authEndpoint ?: throw IllegalStateException("Auth endpoint must be set"),
+    tokenEndpoint = WebasystAuthService.configuration.tokenEndpoint ?: throw IllegalStateException("Token endpoint must be set"),
+    callbackUri = WebasystAuthService.configuration.callbackUri ?: throw IllegalStateException("Callback uri must be set")
+)
