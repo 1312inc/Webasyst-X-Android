d1bdd344d48bbffa6a96a7789cf118cf07c218e1
Nikita Danilov <mirt@mirt.su>
2020-09-01 09:15:32 +0300

    Create CroppingImageView

---
+++ webasyst-x/src/main/java/view/ClippingImageView.kt
@@ -0,0 +1,32 @@
+package com.webasyst.x.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Path
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+/**
+ * This custom [android.widget.ImageView] is clipped by circle.
+ */
+class ClippingImageView : AppCompatImageView {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+        super(context, attrs, defStyleAttr)
+
+    private val clipPath = Path()
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        clipPath.reset()
+        clipPath.addCircle(width / 2f, height / 2f, (width + height) / 4f, Path.Direction.CW)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.save()
+        canvas.clipPath(clipPath)
+        super.onDraw(canvas)
+        canvas.restore()
+    }
+}
