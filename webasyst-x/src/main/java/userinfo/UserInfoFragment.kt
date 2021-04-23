8113b1936a1c4d3f727550e3a776bd81dc8989f6
Nikita Danilov <mirt@mirt.su>
2021-04-23 23:16:35 +0300

    Use Glide to load userpic

--- webasyst-x/src/main/java/userinfo/UserInfoFragment.kt
+++ webasyst-x/src/main/java/userinfo/UserInfoFragment.kt
@@ -7,19 +7,12 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.observe
+import com.bumptech.glide.Glide
 import com.webasyst.x.R
 import com.webasyst.x.WebasystXApplication
 import com.webasyst.x.databinding.NavHeaderAuthorizedBinding
-import com.webasyst.x.util.USERPIC_FILE
-import com.webasyst.x.util.decodeBitmap
-import com.webasyst.x.util.getCacheFile
 import kotlinx.android.synthetic.main.nav_header_authorized.userpicView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.File
 
 class UserInfoFragment : Fragment() {
     private val waidClient by lazy(LazyThreadSafetyMode.NONE) {
@@ -53,19 +46,12 @@ class UserInfoFragment : Fragment() {
             if (url.isEmpty()) {
                 userpicView.setImageResource(R.drawable.ic_userpic_placeholder)
             } else {
-                lifecycleScope.launch(Dispatchers.Main) {
-                    val userpicFile = requireContext().getCacheFile(USERPIC_FILE)
-                    try {
-                        if (!userpicFile.exists() ||
-                            userpicFile.lastModified() + MAX_USERPIC_AGE < System.currentTimeMillis()
-                        ) {
-                            waidClient.downloadUserpic(url, userpicFile)
-                        }
-                        updateUserpicFromFile(userpicFile)
-                    } catch (e: Throwable) {
-                        updateUserpicFromFile(userpicFile)
-                    }
-                }
+                Glide
+                    .with(this)
+                    .load(url)
+                    .circleCrop()
+                    .placeholder(R.drawable.ic_userpic_placeholder)
+                    .into(userpicView)
             }
         }
     }
@@ -74,16 +60,6 @@ class UserInfoFragment : Fragment() {
         super.onResume()
 
         viewModel.updateUserInfo()
-    }
-
-    private suspend fun updateUserpicFromFile(userpicFile: File) {
-        if (userpicFile.exists()) {
-            userpicView.setImageBitmap(withContext(Dispatchers.Default) {
-                userpicFile.decodeBitmap(userpicView)
-            })
-        } else {
-            userpicView.setImageResource(R.drawable.ic_userpic_placeholder)
-        }
     }
 
     companion object {
