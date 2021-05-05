7fc5e9962e306b1d91d0b55f6baf8c96fa832a91
Nikita Danilov <mirt@mirt.su>
2021-05-05 18:22:24 +0300

    Observe installation list changes

--- webasyst-x/src/main/java/intro/LoadingFragment.kt
+++ webasyst-x/src/main/java/intro/LoadingFragment.kt
@@ -1,6 +1,23 @@
 package com.webasyst.x.intro
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.webasyst.x.R
+import com.webasyst.x.installations.InstallationListViewModel
 
-class LoadingFragment : Fragment(R.layout.frag_loading)
+class LoadingFragment : Fragment(R.layout.frag_loading) {
+    val viewModel by lazy(LazyThreadSafetyMode.NONE) {
+        ViewModelProvider(requireActivity()).get(InstallationListViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.state.observe(viewLifecycleOwner) {
+            Log.d("frag_loading", "Installation list state: $it")
+        }
+    }
+}
